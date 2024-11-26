09d8cf05b4ff61c9413b58238963a3260ba9ebb2
sebastiancaba <se.caba@duocuc.cl>
2024-11-26 00:50:25 -0300

    Change server port to 3000

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const cors = require('cors');
 const router = jsonServer.router("almacen.json");
 const middlewares= jsonServer.defaults();
-const port= process.env.PORT || 10000; //web service
+const port= process.env.PORT || 3000; //web service
 
-
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 server.listen(port);
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`);
+  });
